20a4f8dd68832a5234f6b3c2dc38f3b869e401d5
VirrageS <virrages@gmail.com>
2015-06-13 16:42:12 +0200

    Updates in login manager

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import sqlalchemy
 from flask import Flask, Response, request, render_template, url_for, redirect
 
+from flask.ext.login import LoginManager
+
+# App setup
 app = Flask(__name__)
 app.debug = True
 
+# Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Main path - layout
 @app.route("/")
 def layout():
     return render_template('layout.html')
 
+# Something
 @app.route("/awesome")
 def awesome():
 	return render_template('awesome.html')
