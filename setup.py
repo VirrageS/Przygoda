afb7d279fcc7fbbb028c43dd0a343b45c421ea9f
VirrageS <virrages@gmail.com>
2015-06-13 18:20:49 +0200

    Login

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='Przygoda',
-      version='1.0',
-      description='Aplikacja dla rowerzystów poszukujących przygód',
-      author='Janusz Marcinkiewicz',
-      author_email='',
-      url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.7.2', 'MarkupSafe' , 'Flask-SQLAlchemy>=0.16'],
-     )
+	  version='1.0',
+	  description='Aplikacja dla rowerzystów poszukujących przygód',
+	  author='Janusz Marcinkiewicz',
+	  author_email='',
+	  url='http://www.python.org/sigs/distutils-sig/',
+	 install_requires=['Flask>=0.7.2', 'MarkupSafe' , 'Flask-SQLAlchemy>=0.16'],
+	 )
