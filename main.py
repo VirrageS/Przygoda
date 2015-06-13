039c0a61d00b78096e01ff7e9cd3c28871e0cca6
VirrageS <virrages@gmail.com>
2015-06-13 17:38:55 +0200

    Login and register functions

--- main.py
+++ main.py
@@ -1,18 +1,78 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, Response, request, render_template, url_for, redirect
+from datetime import datetime
+import builtins
+import os
+
+from sqlalchemy import create_engine
+from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
+from flask.ext.login import login_user , logout_user , current_user , login_required
 
 from flask.ext.login import LoginManager
+from flask.ext.sqlalchemy import SQLAlchemy
 
-# App setup
+# Unicode bug fix
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+
+# DEBUG
+app.debug = True
+
+engine = create_engine('sqlite:///:memory:', echo=True)
 
 # Database setup
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on' , db.DateTime)
+
+    def __init__(self , username ,password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 # Login setup
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 # Main path - layout
 @app.route("/")
@@ -40,11 +100,26 @@ def register():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username,password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
+	db.create_all()
 
-    app.debug = True
-    app.run()
+	# It is probably unsafe
+	app.secret_key = 'super secret key'
+	app.config['SESSION_TYPE'] = 'filesystem'
+
+	app.run()
