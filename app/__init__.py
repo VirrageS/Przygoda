e7b5c9deed384eea631934825b5340f61c05b9fa
VirrageS <VirrageS@MacBook-Air-Janusz.local>
2015-06-14 18:18:41 +0200

    Few changes

--- app/__init__.py
+++ app/__init__.py
@@ -14,27 +14,27 @@ db = SQLAlchemy(app)
 # Configure Secret Key #
 ########################
 def install_secret_key(app, filename='secret_key'):
-    """Configure the SECRET_KEY from a file
-    in the instance directory.
+	"""Configure the SECRET_KEY from a file
+	in the instance directory.
 
-    If the file does not exist, print instructions
-    to create it from a shell with a random key,
-    then exit.
-    """
-    filename = os.path.join(app.instance_path, filename)
+	If the file does not exist, print instructions
+	to create it from a shell with a random key,
+	then exit.
+	"""
+	filename = os.path.join(app.instance_path, filename)
 
-    try:
-        app.config['SECRET_KEY'] = open(filename, 'rb').read()
-    except IOError:
-        print('Error: No secret key. Create it with:')
-        full_path = os.path.dirname(filename)
-        if not os.path.isdir(full_path):
-            print('mkdir -p {filename}'.format(filename=full_path))
-        print('head -c 24 /dev/urandom > {filename}'.format(filename=filename))
-        sys.exit(1)
+	try:
+		app.config['SECRET_KEY'] = open(filename, 'rb').read()
+	except IOError:
+		print('Error: No secret key. Create it with:')
+		full_path = os.path.dirname(filename)
+		if not os.path.isdir(full_path):
+			print('mkdir -p {filename}'.format(filename=full_path))
+		print('head -c 24 /dev/urandom > {filename}'.format(filename=filename))
+		sys.exit(1)
 
 if not app.config['DEBUG']:
-    install_secret_key(app)
+	install_secret_key(app)
 
 
 
@@ -46,11 +46,11 @@ login_manager.login_view = 'login'
 from app.users.models import User
 @login_manager.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+	return User.query.get(int(id))
 
 @app.before_request
 def before_request():
-    g.user = current_user
+	g.user = current_user
 
 # Google maps
 from flask.ext.googlemaps import GoogleMaps
@@ -58,7 +58,7 @@ GoogleMaps(app)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 from app.users.views import mod as usersModule
 app.register_blueprint(usersModule)
