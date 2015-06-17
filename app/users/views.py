e7a75290a21ded85f29b0b30020b8a6d0413ec72
VirrageS <virrages@gmail.com>
2015-06-17 14:44:26 +0200

    Nowe logowanie i rejestracja

--- app/users/views.py
+++ app/users/views.py
@@ -8,63 +8,6 @@ from app.users.forms import RegisterForm, LoginForm
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
-# Register
-@mod.route('/register/' , methods=['GET','POST'])
-def register():
-	if request.method == 'GET':
-		return render_template('users/register.html')
-
-	username = request.form['username']
-	password = request.form['password']
-	email = request.form['email']
-
-	# @todo: check if request forms are filled properly
-
-	# check for error
-	error = False
-
-	# username was not provided
-	if username is None or (username == ''):
-		flash('No username')
-		error = True
-
-	# password was not provided
-	if password is None or (password == ''):
-		flash('No password')
-		error = True
-
-	# email was not provided
-	if (email is None) or (email == ''):
-		flash('No email')
-		error = True
-
-	# error occured so we back
-	if error:
-		return render_template('users/register.html')
-
-	# check if user with name exists
-	checkUser = User.query.filter_by(username=username).first()
-
-	# check if users with email exists
-	if checkUser is None:
-		checkUser = User.query.filter_by(email=email).first()
-
-	# user with username exists
-	if checkUser != None:
-		flash('User with provided username or email arleady exists')
-		return render_template('users/register.html')
-
-	# create new user
-	user = User(username, generate_password_hash(password), email)
-
-	# add user to database
-	db.session.add(user)
-	db.session.commit()
-
-	# everything okay so back
-	flash('User successfully registered')
-	return redirect(url_for('users.login'))
-
 # Login
 @mod.route('/login/', methods=['GET','POST'])
 def login():
@@ -80,7 +23,7 @@ def login():
 	if form.validate_on_submit():
 		registered_user = User.query.filter_by(username=form.username.data).first()
 
-		if registered_user: #and check_password_hash(registered_user.password, form.password.data):
+		if registered_user and check_password_hash(registered_user.password, form.password.data):
 			# login user to system
 			login_user(registered_user, remember=form.remember_me.data)
 
@@ -89,9 +32,42 @@ def login():
 
 		flash('Wrong username or password', 'error-message')
 
-
-
 	return render_template('users/login.html', form=form)
+
+# Register
+@mod.route('/register/', methods=['GET','POST'])
+def register():
+	# if register form is submitted
+	form = RegisterForm(request.form)
+
+	if request.method == 'GET':
+		return render_template('users/register.html', form=form)
+
+	# verify the register form
+	if form.validate_on_submit():
+		# check if user with name exists
+		checkUser = User.query.filter_by(username=form.username.data).first()
+
+		# check if users with email exists
+		if checkUser is None:
+			checkUser = User.query.filter_by(email=form.email.data).first()
+
+		# user with username exists
+		if checkUser != None:
+			flash('User with provided username or email arleady exists', 'error-message')
+			return render_template('users/register.html', form=form)
+
+		user = User(form.username.data, generate_password_hash(form.email.data), form.email.data)
+
+		# add user to database
+		db.session.add(user)
+		db.session.commit()
+
+		# everything okay so back
+		flash('User successfully registered')
+		return redirect(url_for('users.login'))
+
+	return render_template('users/register.html', form=form)
 
 # Logout
 @mod.route('/logout/')
