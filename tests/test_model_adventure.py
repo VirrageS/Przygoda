b5e7bef721d631c296bd51d68411f8dfcbdcc2e1
VirrageS <virrages@gmail.com>
2015-06-17 16:14:21 +0200

    Poprawki w testach

--- tests/test_model_adventure.py
+++ tests/test_model_adventure.py
@@ -12,15 +12,15 @@ class AdventureTestCase(unittest.TestCase):
 		self.app = app.test_client()
 
 	def test_adventure_date(self):
-		a = Adventure(user='john', date=datetime.utcnow(), info='Some info today', joined=10)
+		a = Adventure(user_id=1, date=datetime.utcnow(), info='Some info today', joined=10)
 		assert a.date != datetime.utcnow()
 
 	def test_adventure_info(self):
-		a = Adventure(user='john', date=datetime.utcnow(), info='Some info today', joined=10)
+		a = Adventure(user_id=2, date=datetime.utcnow(), info='Some info today', joined=10)
 		assert a.info == 'Some info today'
 
 	def test_adventure_joined(self):
-		a = Adventure(user='john', date=datetime.utcnow(), info='Some info today', joined=10)
+		a = Adventure(user_id=3, date=datetime.utcnow(), info='Some info today', joined=10)
 		assert a.joined == 10
 
 if __name__ == '__main__':
