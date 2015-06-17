f878e433752aed010ef33f073d36aa25fa7f304a
VirrageS <VirrageS@MacBook-Air-Janusz.local>
2015-06-17 08:24:49 +0200

    Nowy typ loginu

--- app/users/views.py
+++ app/users/views.py
@@ -4,6 +4,7 @@ from flask.ext.login import LoginManager, login_user, logout_user, current_user,
 
 from app import db
 from app.users.models import User
+from app.users.forms import RegisterForm, LoginForm
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
@@ -54,7 +55,7 @@ def register():
 		return render_template('users/register.html')
 
 	# create new user
-	user = User(username, password, email)
+	user = User(username, generate_password_hash(password), email)
 
 	# add user to database
 	db.session.add(user)
@@ -69,34 +70,28 @@ def register():
 def login():
 	"""Handels login path"""
 
+	# If sign in form is submitted
+	form = LoginForm(request.form)
+
 	if request.method == 'GET':
-		return render_template('users/login.html')
+		return render_template('users/login.html', form=form)
 
-	# get username and password from text fields in form
-	username = request.form['username']
-	password = request.form['password']
+	# Verify the sign in form
+	if form.validate_on_submit():
+		registered_user = User.query.filter_by(username=form.username.data).first()
 
-	remember_me = False
-	if 'remember_me' in request.form:
-		remember_me = True
+		if registered_user: #and check_password_hash(registered_user.password, form.password.data):
+			# login user to system
+			login_user(registered_user, remember=form.remember_me.data)
 
-	# get user from database
-	registered_user = User.query.filter_by(
-		username=username,
-		password=password
-	).first()
+			flash('Logged in successfully')
+			return redirect(request.args.get('next') or url_for('simple_page.index'))
 
-	# failed to get user
-	if registered_user is None:
-		flash('Username or Password is invalid', 'error')
-		return redirect(url_for('users.login'))
+		flash('Wrong username or password', 'error-message')
 
-	# login user to system
-	login_user(registered_user, remember=remember_me)
 
-	# everything okay so back
-	flash('Logged in successfully')
-	return redirect(request.args.get('next') or url_for('simple_page.index'))
+
+	return render_template('users/login.html', form=form)
 
 # Logout
 @mod.route('/logout/')
