49204228caf1cd9dcd7b057cdb24347f9aea2ac2
VirrageS <virrages@gmail.com>
2015-07-15 13:35:52 +0200

    Minor change

--- deploy/afterinstall.sh
+++ deploy/afterinstall.sh
@@ -24,8 +24,8 @@ pip3 install -r requirements.txt;
 deactivate;
 
 sudo touch /etc/init/$PROJECT_NAME.conf;
-echo '
-description "Gunicorn application server running $PROJECT_NAME"
+echo "
+description \"Gunicorn application server running $PROJECT_NAME\"
 
 start on runlevel [2345]
 stop on runlevel [!2345]
@@ -37,14 +37,14 @@ setgid www-data
 env PATH=/home/$USER/$PROJECT_NAME/env/bin
 chdir /home/$USER/$PROJECT_NAME
 exec gunicorn --workers $WORKERS --bind unix:$PROJECT_NAME.sock -m 007 run:app
-' | sudo tee --append /etc/init/$PROJECT_NAME.conf > /dev/null
+" | sudo tee --append /etc/init/$PROJECT_NAME.conf > /dev/null
 
 
 sudo rm -rf /etc/nginx/sites-enabled/default;
 sudo rm -rf /etc/nginx/sites-available/default;
 
 sudo touch /etc/nginx/sites-available/$PROJECT_NAME;
-echo '
+echo "
 server {
     listen 80;
     server_name $IP;
@@ -56,7 +56,7 @@ server {
         proxy_read_timeout 30s;
     }
 }
-' | sudo tee --append /etc/nginx/sites-available/$PROJECT_NAME > /dev/null
+" | sudo tee --append /etc/nginx/sites-available/$PROJECT_NAME > /dev/null
 
 sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled;
 sudo nginx -t;
