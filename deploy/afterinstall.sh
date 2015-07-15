cbf0c016aff7634e4163ee3c4bbb9cd5c4e6b324
VirrageS <virrages@gmail.com>
2015-07-15 20:18:35 +0200

    Minor changes

--- deploy/afterinstall.sh
+++ deploy/afterinstall.sh
@@ -24,8 +24,7 @@ pip3 install -r requirements.txt;
 deactivate;
 
 sudo touch /etc/init/$PROJECT_NAME.conf;
-echo "
-description \"Gunicorn application server running $PROJECT_NAME\"
+echo "description \"Gunicorn application server running $PROJECT_NAME\"
 
 start on runlevel [2345]
 stop on runlevel [!2345]
@@ -36,27 +35,23 @@ setgid www-data
 
 env PATH=/home/$USER/$PROJECT_NAME/env/bin
 chdir /home/$USER/$PROJECT_NAME
-exec gunicorn --workers $WORKERS --bind unix:$PROJECT_NAME.sock -m 007 run:app
-" | sudo tee --append /etc/init/$PROJECT_NAME.conf > /dev/null
-
+exec gunicorn --workers $WORKERS --bind unix:$PROJECT_NAME.sock -m 007 run:app" | sudo tee --append /etc/init/$PROJECT_NAME.conf > /dev/null
 
 sudo rm -rf /etc/nginx/sites-enabled/default;
 sudo rm -rf /etc/nginx/sites-available/default;
 
 sudo touch /etc/nginx/sites-available/$PROJECT_NAME;
-echo "
-server {
+echo "server {
     listen 80;
     server_name $IP;
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:/home/$USER/$PROJECT_NAME/przygoda.sock;
-        proxy_connect_timeout 30s;
-        proxy_read_timeout 30s;
+        proxy_pass http://unix:/home/$USER/$PROJECT_NAME/$PROJECT_NAME.sock;
+        proxy_connect_timeout 300s;
+        proxy_read_timeout 300s;
     }
-}
-" | sudo tee --append /etc/nginx/sites-available/$PROJECT_NAME > /dev/null
+}" | sudo tee --append /etc/nginx/sites-available/$PROJECT_NAME > /dev/null
 
 sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled;
 sudo nginx -t;
