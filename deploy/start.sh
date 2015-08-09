14bb4c1615f5e6dae9bb9e951f92589e117dc6ef
VirrageS <virrages@gmail.com>
2015-08-09 09:21:05 +0200

    Minor changes in UX and in deployment

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# run redis
-sudo ./run-redis.sh
-
-# start celery
-. ./env/bin/activate;
-celery worker -A app.celery --loglevel=info
-deactivate;
-
 # start app
 sudo start przygoda;
 sudo service nginx restart;
