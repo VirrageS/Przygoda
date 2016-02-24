b44d795089c6d5ad03f3de7645363a7ba0eb9a7d
VirrageS <virrages@gmail.com>
2016-02-24 20:33:57 +0100

    Fix: user permissions

--- deploy/afterinstall.sh
+++ deploy/afterinstall.sh
@@ -86,6 +86,5 @@ deactivate;
 sudo chmod a+x run-redis.sh
 sudo chmod a+x run-celery.sh
 
-cd ..;
-sudo chown ubuntu:ubuntu $PROJECT_NAME/;
-sudo chmod -R 777 $PROJECT_NAME/;
+sudo chown ubuntu:ubuntu *;
+sudo chmod -R 777 *;
